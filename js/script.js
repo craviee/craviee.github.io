926eefa608e9fccbffcadeb4f51a3e338cba5177
Daniel Craviée <craviee@pm.me>
2021-02-04 08:35:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,50 +6,39 @@ app.controller('scriptController', function($scope)
     let links =
     [
         {
-            name: "IHM",
+            name: "Work",
             items:
             [
-                ["E-mail", "https://outlook.office.com/mail/inbox"],
+                ["Outlook", "https://outlook.office.com/mail/inbox"],
+                ["MS Teams", "https://teams.microsoft.com/"],
                 ["IHM Systems", "http://sistemas.ihm.com.br"],
-                ["GlassFrog","https://app.glassfrog.com/"],
                 ["Azure DevOps","https://dev.azure.com/"],
+                ["GlassFrog","https://app.glassfrog.com/"],
                 ["SGI", "http://sgi.ihm.com.br"]
             ]
         },
-        {
-            name: "PUT",
-            items:
-            [
-                ["E-mail", "https://poczta.student.put.poznan.pl/"],
-                ["eLogin", "https://estudent2.put.poznan.pl"],
-                ["Moodle", "https://moodle.put.poznan.pl/"]
-            ]
-        },
-
         {
             name: "Social",
             items:
             [
                 ["Prontonmail", "https://mail.protonmail.com/login"],
-                ["LinkedIn", "https://www.linkedin.com/in/craviee/"],
-                ["Github", "https://github.com/"]
+                ["Whatsapp", "https://web.whatsapp.com/"],
+                ["Discord", "https://discord.com/login"]
             ]
         },
-
         {
             name: "Stuff",
             items:
             [
                 ["Saco Cheio TV", "https://www.sacocheio.tv/login.php"],
+                ["Radio Garden","http://radio.garden/"],
                 ["Library Genesis", "http://gen.lib.rus.ec/"],
                 ["Pomodoro", "https://tomato-timer.com/"],
                 ["DrawIO", "https://www.draw.io/"],
-                ["YTMP3","https://ytmp3.cc/"],
-                ["Radio Garden","http://radio.garden/visit/belo-horizonte-mg/"],
+                ["YTMP3","https://ytmp3.cc/"],   
                 ["Free Games", "https://isthereanydeal.com/specials/#/filter:&giveaway,&active"]
             ]
         },
-
     ];
     $scope.links = links;
 });
